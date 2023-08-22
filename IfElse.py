8d0f167ac05f47ccc878521e809f85cde002ce2a
RDX-Rudra <129542812+RDX-Rudra@users.noreply.github.com>
2023-08-22 23:35:47 +0530

    Add files via upload

---
+++ IfElse.py
@@ -0,0 +1,10 @@
+a = int(input("Enter your age: "))
+print("your age is: ",a)
+
+if(a>18):
+    print("you can vote")
+elif(a==18):
+    print("you are new voter")
+else:
+    print("you cann't vote")
+print("Thankyou")
---
+++ String.py
@@ -0,0 +1,11 @@
+massage = "What do you like?"
+response = 'spam'
+print(massage)
+print(response)
+print(len(response))
+print(response.upper())
+print(massage.lower())
+print(massage.capitalize())
+print(massage + response)
+print(5 * response)
+print(massage[0])
